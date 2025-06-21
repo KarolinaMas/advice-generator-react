18a371673bb16fa80c44a861563bc81472cb9336
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-06-21 15:32:06 +0300

    add styling for tablet and desktop screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import dividerPhone from "./assets/pattern-divider-mobile.svg";
+import dividerDesktop from "./assets/pattern-divider-desktop.svg";
 import diceIcon from "../public/icon-dice.svg";
 
 const App = () => {
@@ -10,7 +11,6 @@ const App = () => {
       const data = await fetch("https://api.adviceslip.com/advice");
       const response = await data.json();
       setAdvice(response.slip);
-      console.log(advice);
     } catch (error) {
       setAdvice(`Failed to fetch advice - ${error}, please try again later`);
     }
@@ -21,14 +21,19 @@ const App = () => {
   }, []);
 
   return (
-    <div className="bg-[#4F5D74]/40 mx-auto max-w-[343px] h-[314px] rounded-[10px] text-center px-6 py-8 relative">
-      <p className="text-[#53FFAA] text-[13px] leading-[1.35] tracking-[4px] font-extrabold mb-4">
+    <div className="bg-[#4F5D74]/40 mx-auto max-w-[343px] h-[314px] rounded-[10px] text-center px-6 py-8 relative sm:max-w-[540px] sm:h-[332px] sm:rounded-2xl sm:px-12 sm:py-12">
+      <p className="text-[#53FFAA] text-[13px] leading-[1.35] tracking-[4px] font-extrabold">
         ADVICE #{advice.id}
       </p>
-      <p className="text-[#CEE3E9] text-2xl leading-[1.35] font-extrabold tracking-[-0.3px]">
+      <p className="text-[#CEE3E9] text-2xl leading-[1.35] font-extrabold tracking-[-0.3px] mt-4 sm:text-[28px] sm:mt-6">
         {advice.advice}
       </p>
-      <img src={dividerPhone} alt="deco image." className="mt-6" />
+      <img
+        src={dividerPhone}
+        srcSet={`${dividerDesktop} 640w`}
+        alt="deco image."
+        className="mt-6"
+      />
       <button
         className="bg-[#53FFAA] w-[64px] h-[64px] align-center rounded-4xl absolute bottom-0 left-1/2 translate-x-[-50%] translate-y-1/2 hover:shadow-[0px_0px_32px_10px_rgba(83,255,170,0.4)] cursor-pointer"
         onClick={getAdvice}
